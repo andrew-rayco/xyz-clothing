5af222ad0235987470c0cdfe8f0a62afe8dd111d
Andrew <andy@andycondon.com>
2020-03-14 17:49:04 +1300

    Adds SingleProduct component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 import ProductList from './ProductList'
+import SingleProduct from './SingleProduct'
 
 import products from './data/products.json'
 
@@ -10,6 +11,9 @@ export default () => (
     <div className="app">
       <h1>XYZ Clothing</h1>
       <Switch>
+        <Route path="/products/:id">
+          <SingleProduct />
+        </Route>
         <Route path="/" exact>
           <ProductList products={products} />
         </Route>
---
+++ src/SingleProduct.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const SingleProduct = props => {
+  return <div>Check out how SingleProduct I am</div>
+}
+
+export default SingleProduct
