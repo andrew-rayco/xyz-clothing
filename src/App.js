de2d6acfdf99b725c208939b382aa242faf1bd9a
Andrew <andy@andycondon.com>
2020-03-14 17:19:35 +1300

    Adds react router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 import ProductList from './ProductList'
 
 import products from './data/products.json'
 
 export default () => (
-  <>
-    <h1>XYZ Clothing</h1>
-    <ProductList products={products} />
-  </>
+  <Router>
+    <div className="app">
+      <h1>XYZ Clothing</h1>
+      <Switch>
+        <Route path="/" exact>
+          <ProductList products={products} />
+        </Route>
+      </Switch>
+    </div>
+  </Router>
 )
