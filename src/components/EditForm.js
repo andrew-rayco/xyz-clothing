ad6db814329574fba04e97d99d5066f39dd73d49
Andrew <andy@andycondon.com>
2020-03-16 16:03:26 +1300

    Styles form, adds react-select

--- src/components/EditForm.js
+++ src/components/EditForm.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react'
 import { Link, withRouter } from 'react-router-dom'
 import PropTypes from 'prop-types'
+import Select from 'react-select'
+
+import { calculateLocalPrice } from '../utils'
+
+const selectOptions = [
+  { value: 'AUD', label: 'AUD' },
+  { value: 'USD', label: 'USD' },
+  { value: 'CNY', label: 'CNY' }
+]
 
 class EditForm extends Component {
   constructor(props) {
@@ -13,9 +22,9 @@ class EditForm extends Component {
         price: {
           base: '',
           amount: ''
-        }
+        },
+        relatedProducts: []
       },
-      relatedProducts: [],
       productIndex: ''
     }
   }
@@ -55,10 +64,26 @@ class EditForm extends Component {
     return products
   }
 
-  handleChange(e) {
+  handleChange(e, selectName) {
     const { target } = e
-    const name = target.name
-    const value = name === 'id' ? parseInt(target.value) : target.value
+
+    let name, value
+    if (selectName) {
+      // Handles select-react output from currency dropdown list
+      name = selectName.split('-')[1]
+      value = e.value
+
+      this.setState({
+        product: {
+          ...this.state.product,
+          price: { ...this.state.product.price, [name]: value }
+        }
+      })
+      console.log('Change detected. State updated ' + name + ' = ' + value)
+    } else {
+      name = target.name
+      value = name === 'id' ? parseInt(target.value) : target.value
+    }
 
     this.setState({
       product: {
@@ -69,71 +94,106 @@ class EditForm extends Component {
     console.log('Change detected. State updated ' + name + ' = ' + value)
   }
 
+  handleSelect(e) {
+    const { base, amount } = this.state.product.price
+    const value = e.value
+    const newPrice = calculateLocalPrice(base, parseInt(amount), value)
+
+    this.setState({
+      product: {
+        price: {
+          base: value,
+          price: newPrice
+        }
+      }
+    })
+  }
+
   handleSubmit(e) {
     e.preventDefault()
     const newAllProducts = this.mergeProducts()
-    // Sends to localStorage and Application state.
     this.props.updateProducts(newAllProducts)
-
     this.props.history.push('/')
   }
 
   render() {
-    const { id, name, description, price } = this.state.product
+    const { id, name, description, price, relatedProducts } = this.state.product
     return (
       <div className="edit-form">
+        <h2>Edit {name}</h2>
         <form onSubmit={e => this.handleSubmit(e)}>
-          <div className="form-group">
-            <label htmlFor="id">id</label>
-            <input
-              type="number"
-              name="id"
-              value={id}
-              onChange={e => this.handleChange(e)}
-              placeholder="id"
-            />
-          </div>
+          <div className="form-fields">
+            <div className="form-left">
+              <div className="form-group">
+                <label htmlFor="name">Name</label>
+                <input
+                  name="name"
+                  type="text"
+                  value={name}
+                  onChange={e => this.handleChange(e)}
+                  placeholder="Name"
+                />
+              </div>
 
-          <div className="form-group">
-            <label htmlFor="name">Name</label>
-            <input
-              name="name"
-              type="text"
-              value={name}
-              onChange={e => this.handleChange(e)}
-              placeholder="Name"
-            />
-          </div>
+              <div className="form-group">
+                <label htmlFor="description">Description</label>
+                <textarea
+                  name="description"
+                  type="text"
+                  value={description}
+                  onChange={e => this.handleChange(e)}
+                  placeholder="Description"
+                  rows="3"
+                />
+              </div>
 
-          <div className="form-group">
-            <label htmlFor="description">Description</label>
-            <textarea
-              name="description"
-              type="text"
-              value={description}
-              onChange={e => this.handleChange(e)}
-              placeholder="Description"
-            />
-          </div>
+              <div className="form-group">
+                <label htmlFor="related">Related</label>
+                <input
+                  name="related"
+                  type="text"
+                  value={relatedProducts}
+                  onChange={e => this.handleChange(e)}
+                  placeholder="Select related"
+                />
+              </div>
+            </div>
 
-          <div className="form-group">
-            <label htmlFor="price-amount">Price Amount</label>
-            <input
-              name="price-amount"
-              type="number"
-              value={price.amount}
-              onChange={e => this.handleChange(e)}
-              placeholder="Price amount"
-            />
+            <div className="form-right">
+              <div className="form-group">
+                <label htmlFor="price-amount">Price Amount</label>
+                <input
+                  name="price-amount"
+                  type="number"
+                  value={price.amount}
+                  onChange={e => this.handleChange(e)}
+                  placeholder="Price amount"
+                />
+              </div>
 
-            <label htmlFor="price-base">Price Base</label>
-            <input
-              name="price-base"
-              type="text"
-              value={price.base}
-              onChange={e => this.handleChange(e)}
-              placeholder="Base price currency"
-            />
+              <div className="form-group">
+                <label htmlFor="price-base">Price Base</label>
+                <Select
+                  value={{ label: price.base }}
+                  onChange={e => this.handleChange(e, 'price-base')}
+                  options={selectOptions}
+                  className="rs-container"
+                  classNamePrefix="rs"
+                  name="price-base"
+                />
+              </div>
+
+              <div className="form-group">
+                <label htmlFor="id">id</label>
+                <input
+                  type="number"
+                  name="id"
+                  value={id}
+                  onChange={e => this.handleChange(e)}
+                  placeholder="id"
+                />
+              </div>
+            </div>
           </div>
           <p>Related products select field?</p>
           <input type="submit" value="Submit" />
